085beb5a193592b650c516e295c36b27e89fafaf
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-12 23:49:19 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("Content Loaded bro");
+    showMovies()
+})
+
+function showMovies(){
+    fetch(' http://localhost:3000/films')
+    .then(resp=>resp.json())
+    .then(movies => {
+        console.log(movies)
+    })
+}
