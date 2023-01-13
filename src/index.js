b764a76bd10599666ca732f04eec5fcd3d506cba
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-13 11:23:59 +0300

    button activated

--- src/index.js
+++ src/index.js
@@ -12,3 +12,15 @@ function showMovies(){
         console.log(movies)
     })
 }
+
+let button = document.getElementById('buy-ticket');
+button.addEventListener('click',function(){
+    console.log("button works")
+})
+
+// add new movie to left menu
+let films = document.getElementById("films");
+newListItem = document.createElement("li")
+newListItem.classList.add("film", "item");
+newListItem.innerText = "New Movie Title";
+films.appendChild(newListItem);
