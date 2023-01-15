b4270ea2fa9b769d0d0e2b8c0cf3bd209cf8c159
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-15 22:47:01 +0300

    gitMerge branch 'petersamson'

---
+++ index2.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+// Button click functionality
+let button = document.getElementById('buy-ticket');
+button.addEventListener('click',function(){
+    console.log("button works")
+})
+
+// add new movie to left menu
+
+//function to get movie name index withing the object and add the nodes to the parent element
+// let films = document.getElementById("films");
+// newListItem = document.createElement("li")
+// newListItem.classList.add("film", "item");
+// newListItem.innerText = "New Movie Title";
+// films.appendChild(newListItem);
+
+
+
+// var link = document.getElementById("my-link");
+
+// // Add an event listener to the link
+// link.addEventListener("click", function() {
+//   // Make the GET request
+//   fetch("https://example.com")
+//     .then(response => response.text())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+// });
