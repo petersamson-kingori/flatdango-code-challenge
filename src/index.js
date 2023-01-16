9be50619647cc122b42238bc5d88429916bf6dc1
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-16 17:12:06 +0300

    button functionality Working[Challenge Completed]

--- src/index.js
+++ src/index.js
@@ -6,42 +6,70 @@ document.addEventListener("DOMContentLoaded",()=>{
     console.log("Content Loaded bro");
     showMovies()
 })
-
-function changeImage(){
-    fetch('https://my-json-server.typicode.com/petersamson-kingori/flatdango-code-challenge/films')
-    .then(resp=>resp.json())
-    .then(movies => {   
-    const image = document.getElementById('poster')
-    image.src = movies.poster
-    // console.log('link working')
-}
-)
-}
-
-
-
-
 function showMovies(){
-    fetch('https://my-json-server.typicode.com/petersamson-kingori/flatdango-code-challenge/films')
-    .then(resp=>resp.json())
-    .then(movies => {
-        
-        const titles = Array.from(movies, movie => movie.title);    
-        const posters = Array.from(movies, movie => movie.poster);
-        console.log(posters)
-        let films = document.getElementById("films");
-        
+    fetch("https://my-json-server.typicode.com/petersamson-kingori/flatdango-code-challenge/films")
+  .then(response => response.json())
+  .then(data => {
+    let films = data;
+    let filmsList = document.getElementById("films");
+    let poster = document.getElementById("poster");
+    poster.src = "https://www.gstatic.com/tv/thumb/v22vodart/2157/p2157_v_v8_ab.jpg";
 
-        titles.forEach(titles => {
-            const list = document.createElement("li");
-            list.textContent = titles;
-            list.addEventListener('click',changeImage)
-            films.appendChild(list) 
-            list.id = "new-movies"
+    let showing = document.getElementById("showing");
+    let title = document.getElementById("title");
+    let runtime = document.getElementById("runtime");
+    let filmInfo = document.getElementById("film-info");
+    let showtime = document.getElementById("showtime");
+    let ticketNum = document.getElementById("ticket-num");
+    let button = document.getElementById('buy-ticket')
+    button.addEventListener('click', function() {
+        let target = document.querySelector('#ticket-num');
+        let value = parseInt(target.textContent);
+          if (value > 0) {
+          value--;
+          target.textContent = value;
+        }  if (value === 0) {
+          button.disabled = true;
+          button.textContent = "Sold-Out"
         }
-        )
-        
-    }
+      });
+
     
-    )
+
+
+    
+
+
+        for (let i = 0; i < films.length; i++) {
+        let film = films[i];
+        let filmTitle = film.title;
+        let filmPoster = film.poster;
+        let filmItem = document.createElement("li");
+
+        let filmRuntime = film.runtime;
+        let filmShowtime = film.showtime;
+        let filmDescription = film.description;
+        let theaterCapacity = film.capacity;
+        let ticketsSold = film.tickets_sold;
+        let availableTickets = theaterCapacity - ticketsSold;
+
+    filmItem.innerHTML = filmTitle;
+    filmsList.appendChild(filmItem);
+    filmItem.addEventListener("click", function(){
+    poster.src = filmPoster;
+    
+
+
+    title.textContent = filmTitle;
+      runtime.textContent = filmRuntime + " minutes";
+      filmInfo.textContent = filmDescription;
+      showtime.textContent = filmShowtime;
+      ticketNum.textContent = availableTickets;
+
+      
+
+        });
+    }
+  });
+
 }
